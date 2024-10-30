a0876fc1a2fcfad5bd3077af72fe75d0c0b25b27
Stefanus Setiawan <stefanus.setiawan@bankmega.com>
2024-10-30 15:07:56 +0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t silverblaze79/spring-argocd-app:v1 . -f k8s/Dockerfile
+
+docker push silverblaze79/spring-argocd-app:v1
---
+++ src/main/java/com/springboot/springargocd/SpringArgoCdApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.springargocd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@SpringBootApplication
+public class SpringArgoCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArgoCdApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner runner() {
+		return args -> log.info("Application Version: V1");
+	}
+
+}
